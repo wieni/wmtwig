b44994bf32d7b1935bca4d2db89b9e877ccdc624
Dieter Holvoet <dieter.holvoet@gmail.com>
2021-08-30 22:49:16 +0200

    Remove unused service

--- src/EventSubscriber/TemplateParameterCacheableDependencySubscriber.php
+++ src/EventSubscriber/TemplateParameterCacheableDependencySubscriber.php
@@ -4,22 +4,17 @@ namespace Drupal\wmtwig\EventSubscriber;
 
 use Drupal\Core\Cache\CacheableDependencyInterface;
 use Drupal\Core\Cache\CacheableMetadata;
-use Drupal\Core\Render\RendererInterface;
 use Drupal\wmtwig\Event\TemplateParameterEvent;
 use Drupal\wmtwig\WmTwigEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class TemplateParameterCacheableDependencySubscriber implements EventSubscriberInterface
 {
-    /** @var RendererInterface */
-    protected $renderer;
     /** @var CacheableMetadata */
     protected $metadata;
 
-    public function __construct(
-        RendererInterface $renderer
-    ) {
-        $this->renderer = $renderer;
+    public function __construct()
+    {
         $this->metadata = new CacheableMetadata();
     }
 
